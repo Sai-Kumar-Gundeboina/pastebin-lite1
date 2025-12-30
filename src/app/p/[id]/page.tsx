bef2d74d50f1298b5932e3c51eba6b12351ec05d
Sai Kumar Gundeboina <75746273+Sai-Kumar-Gundeboina@users.noreply.github.com>
2025-12-30 09:25:39 +0530

    Update page.tsx

--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -52,7 +52,7 @@ export default async function PastePage(
         )}
         {paste.expires_at && (
           <p>
-            Expires at: {new Date(paste.expires_at).toLocaleString()}
+            Expires at: {new Date(paste.expires_at).toUTCString()}
           </p>
         )}
       </div>
